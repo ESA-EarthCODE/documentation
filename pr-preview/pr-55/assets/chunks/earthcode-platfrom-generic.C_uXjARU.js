1af431b2fdc8694269f2fab93da2a9bb2a3262c4
anders0204 <anders0204@users.noreply.github.com>
2025-04-28 07:34:46 +0000

    Remove preview for PR 55 🛬

--- pr-preview/pr-55/assets/chunks/earthcode-platfrom-generic.C_uXjARU.js
+++
@@ -1,1 +0,0 @@
-const e="/documentation/pr-preview/pr-55/img/terms/earthcode-platfrom-generic.svg";export{e as _};
--- pr-preview/pr-55/assets/chunks/open-science-esa.DHfjpZ7m.js
+++
@@ -1,1 +0,0 @@
-const e="/documentation/pr-preview/pr-55/img/terms/open-science-esa.png";export{e as _};
